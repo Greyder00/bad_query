f5ff53335b3d04d5a7f4055bb65f747bc84a01df
Greyder <guilhermesouza12162005@gmail.com>
2026-09-08 11:53:05 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,26 +4,20 @@ import PackageDescription
 
 let package = Package(
     name: "bad_query",
-
     platforms: [
         .iOS(.v15)
     ],
-
     products: [
         .library(
             name: "bad_query",
             targets: ["bad_query"]
         )
     ],
-
     targets: [
         .target(
             name: "bad_query",
-            path: "bad_query",
-            sources: [
-                "bad_query.c"
-            ],
-            publicHeadersPath: ".",
+            path: "Sources/bad_query",
+            publicHeadersPath: "include",
             linkerSettings: [
                 .linkedLibrary("xpc")
             ]
